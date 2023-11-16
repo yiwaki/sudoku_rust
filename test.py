7fbf0f4a7c22b582769541b65ff2904883b4eb85
Y.Iwaki <y_iwaki@me.com>
2023-11-16 19:37:38 +0900

    modified:   test.py

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 import numpy as np
 from sudoku_rust import bruteforce
 
-FULL_BITS = 2**9 - 1
 
-
-def load_csv(filepath):
+def load_csv(filepath: str) -> None:
     problem = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
     print("Puzzle:")
     print(problem)
     working = (lambda x: 1 << (x - 1))(problem)
-    working = np.where(working == 0, FULL_BITS, working)
+    working = np.where(working == 0, 0b111111111, working)
     return working
+
+
+def display(x: np.ndarray) -> None:
+    print("Solution:")
+    print(np.int_(np.log2(x) + 1))
 
 
 x = load_csv("data/evil_3.csv")
 
 y = bruteforce(x)
-print("Solution:")
-print(np.int_(np.log2(y) + 1))
+
+display(y)
