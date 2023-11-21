b7f42dbded31496c703a03c10bc254632618cfb4
Yasushi Iwaki <you@example.com>
2023-11-21 21:05:07 +0900

    modified:   test.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import numpy as np
+import time
 from sudoku_rust import bruteforce
-
 
 filepath = "data/evil_3.csv"
 x = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
 print("Puzzle:")
 print(x)
 
+start = time.process_time()
 y = bruteforce(x)
+end = time.process_time()
+elapsed = end - start
 
 print("Solution:")
 print(y)
+
+print(f"elapsed time: {elapsed:.6f}")
