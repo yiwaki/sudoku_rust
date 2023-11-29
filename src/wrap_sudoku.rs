3c83fda4e9157985028962fda356514836649926
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2023-11-29 15:27:29 +0900

    modified:   src/wrap_sudoku.rs

--- src/wrap_sudoku.rs
+++ src/wrap_sudoku.rs
@@ -1,4 +1,4 @@
-use numpy::{ndarray::Array, IntoPyArray, PyArray2, PyReadonlyArray2};
+use numpy::{ndarray::arr2, IntoPyArray, PyArray2, PyReadonlyArray2};
 use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 mod sudoku;
@@ -11,28 +11,21 @@ fn sudoku_rust<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         py: Python<'py>,
         arr: PyReadonlyArray2<'py, sudoku::matrix::bitmap::Bitmap>,
     ) -> &'py PyArray2<sudoku::matrix::bitmap::Bitmap> {
-        let arr = arr.as_array().map(|x| {
-            if *x == 0 {
-                sudoku::matrix::bitmap::FULL_BIT
-            } else {
-                (1 << (*x - 1)) as sudoku::matrix::bitmap::Bitmap
-            }
-        });
-        let x = Box::new(
-            [(); sudoku::matrix::MATRIX_SIZE]
-                .map(|()| [(); sudoku::matrix::MATRIX_SIZE].map(|()| *arr.iter().next().unwrap())),
-        );
+        let arr = arr.as_array();
+        let x = Box::new([(); sudoku::matrix::MATRIX_SIZE].map(|()| {
+            [(); sudoku::matrix::MATRIX_SIZE].map(|()| {
+                let z = arr.iter().next().unwrap();
+                if *z == 0 {
+                    sudoku::matrix::bitmap::FULL_BIT
+                } else {
+                    1 << (*z - 1)
+                }
+            })
+        }));
 
         let y = sudoku::bruteforce(&x, 0);
 
-        // let arr = Array2(y);
-        // let arr = arr.map(|x| x)
-        let mut arr = Array::zeros((sudoku::matrix::MATRIX_SIZE, sudoku::matrix::MATRIX_SIZE));
-        for i in 0..sudoku::matrix::MATRIX_SIZE {
-            for j in 0..sudoku::matrix::MATRIX_SIZE {
-                arr[(i, j)] = y[i][j].ilog2() as sudoku::matrix::bitmap::Bitmap + 1;
-            }
-        }
+        let arr = arr2(&*y).map(|&z| z.ilog2() as sudoku::matrix::bitmap::Bitmap + 1);
         arr.into_pyarray(py)
     }
 
