9351f14287ade0eb822f5922d413b60895de593f
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2023-11-29 17:01:10 +0900

    modified:   src/wrap_sudoku.rs

--- src/wrap_sudoku.rs
+++ src/wrap_sudoku.rs
@@ -1,5 +1,5 @@
 use numpy::{ndarray, IntoPyArray, PyArray2, PyReadonlyArray2};
-use pyo3::{pymodule, types::PyModule, PyResult, Python};
+use pyo3::{pyfunction, pymodule, types::PyModule, wrap_pyfunction, PyResult, Python};
 
 mod sudoku;
 
@@ -22,20 +22,21 @@ fn _matrix_to_array(x: &sudoku::matrix::Matrix) -> ndarray::Array2<sudoku::matri
     ndarray::arr2(&**x).map(|z| (*z).ilog2() as sudoku::matrix::bitmap::Bitmap + 1)
 }
 
+#[pyfunction]
+fn bruteforce<'py>(
+    py: Python<'py>,
+    arr: PyReadonlyArray2<'py, sudoku::matrix::bitmap::Bitmap>,
+) -> &'py PyArray2<sudoku::matrix::bitmap::Bitmap> {
+    let x = _array_to_matrix(&arr.as_array());
+
+    let y = sudoku::bruteforce(&x, 0);
+
+    _matrix_to_array(&y).into_pyarray(py)
+}
+
 #[pymodule]
 fn sudoku_rust<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
-    #[pyfn(m)]
-    #[pyo3(name = "bruteforce")]
-    fn bruteforce_py<'py>(
-        py: Python<'py>,
-        arr: PyReadonlyArray2<'py, sudoku::matrix::bitmap::Bitmap>,
-    ) -> &'py PyArray2<sudoku::matrix::bitmap::Bitmap> {
-        let x = _array_to_matrix(&arr.as_array());
-
-        let y = sudoku::bruteforce(&x, 0);
-
-        _matrix_to_array(&y).into_pyarray(py)
-    }
+    m.add_function(wrap_pyfunction!(bruteforce, m)?)?;
 
     Ok(())
 }
