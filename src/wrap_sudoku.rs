8eb24a3f0434245b9cc2015a6c06e76902b924f1
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2023-11-29 16:20:19 +0900

    modified:   src/wrap_sudoku.rs

--- src/wrap_sudoku.rs
+++ src/wrap_sudoku.rs
@@ -3,6 +3,25 @@ use pyo3::{pymodule, types::PyModule, PyResult, Python};
 
 mod sudoku;
 
+fn _array_to_matrix(
+    x: &ndarray::ArrayView2<sudoku::matrix::bitmap::Bitmap>,
+) -> sudoku::matrix::Matrix {
+    Box::new([(); sudoku::matrix::MATRIX_SIZE].map(|()| {
+        [(); sudoku::matrix::MATRIX_SIZE].map(|()| {
+            let z = x.iter().next().unwrap();
+            if *z == 0 {
+                sudoku::matrix::bitmap::FULL_BIT
+            } else {
+                1 << (*z - 1)
+            }
+        })
+    }))
+}
+
+fn _matrix_to_array(x: &sudoku::matrix::Matrix) -> ndarray::Array2<sudoku::matrix::bitmap::Bitmap> {
+    ndarray::arr2(&**x).map(|z| (*z).ilog2() as sudoku::matrix::bitmap::Bitmap + 1)
+}
+
 #[pymodule]
 fn sudoku_rust<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
     #[pyfn(m)]
@@ -11,22 +30,11 @@ fn sudoku_rust<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
         py: Python<'py>,
         arr: PyReadonlyArray2<'py, sudoku::matrix::bitmap::Bitmap>,
     ) -> &'py PyArray2<sudoku::matrix::bitmap::Bitmap> {
-        let arr = arr.as_array();
-        let x = Box::new([(); sudoku::matrix::MATRIX_SIZE].map(|()| {
-            [(); sudoku::matrix::MATRIX_SIZE].map(|()| {
-                let z = arr.iter().next().unwrap();
-                if *z == 0 {
-                    sudoku::matrix::bitmap::FULL_BIT
-                } else {
-                    1 << (*z - 1)
-                }
-            })
-        }));
+        let x = _array_to_matrix(&arr.as_array());
 
         let y = sudoku::bruteforce(&x, 0);
 
-        let arr = ndarray::arr2(&*y).map(|&z| z.ilog2() as sudoku::matrix::bitmap::Bitmap + 1);
-        arr.into_pyarray(py)
+        _matrix_to_array(&y).into_pyarray(py)
     }
 
     Ok(())
