746a411ec7de1974521cb66c8ce405b23c3d2ea8
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2023-12-04 21:07:45 +0900

    modified:   src/sudoku.rs 	modified:   src/sudoku/matrix/bitmap.rs

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -84,9 +84,8 @@ impl matrix::Matrix {
         let mut x = matrix::Matrix::new();
 
         let pivot = matrix::cell_no_to_addr(cell_no);
-        let bits = bitmap::each_bit(self[pivot]);
 
-        for target_bit in bits.into_iter() {
+        for target_bit in bitmap::EachBit::from(self[pivot]).into_iter() {
             x = match self._pruned_by_pivot(pivot, target_bit) {
                 Some(y) => y,
                 None => {
--- src/sudoku/matrix/bitmap.rs
+++ src/sudoku/matrix/bitmap.rs
@@ -3,17 +3,35 @@ pub type Bitmap = u16;
 pub const BITMAP_DIGIT: usize = 9;
 pub const FULL_BIT: Bitmap = 0b1_1111_1111;
 
-pub fn each_bit(bitmap: Bitmap) -> Vec<Bitmap> {
-    let mut each_bit: Vec<Bitmap> = Vec::new();
-    let mut target_bit: Bitmap = 1;
+pub struct EachBit {
+    bit_buffer: Bitmap,
+    current_bit: Bitmap,
+}
 
-    for _ in 0..BITMAP_DIGIT {
-        if bitmap & target_bit != 0 {
-            each_bit.push(target_bit);
+impl EachBit {
+    pub fn from(bitmap: Bitmap) -> Self {
+        EachBit {
+            bit_buffer: bitmap,
+            current_bit: 1,
         }
-        target_bit <<= 1;
     }
-    each_bit
+}
+
+impl Iterator for EachBit {
+    type Item = Bitmap;
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.bit_buffer & self.current_bit == 0 && self.current_bit != 0 {
+            self.current_bit <<= 1;
+        }
+
+        if self.current_bit == 0 {
+            None
+        } else {
+            let cur = self.current_bit;
+            self.current_bit <<= 1;
+            Some(cur)
+        }
+    }
 }
 
 pub fn popcount(bitmap: Bitmap) -> usize {
@@ -35,13 +53,22 @@ mod tests {
 
     #[test]
     fn split_single_bit_test() {
-        let bits = each_bit(0b0_0000_0000);
+        let mut bits = Vec::<Bitmap>::new();
+        for v in EachBit::from(0b0_0000_0000).into_iter() {
+            bits.push(v);
+        }
         assert_eq!(bits, []);
 
-        let bits = each_bit(0b1_0010_0101);
+        let mut bits = Vec::<Bitmap>::new();
+        for v in EachBit::from(0b1_0010_0101).into_iter() {
+            bits.push(v);
+        }
         assert_eq!(bits, [1, 4, 32, 256]);
 
-        let bits = each_bit(0b1_1111_1111);
+        let mut bits = Vec::<Bitmap>::new();
+        for v in EachBit::from(0b1_1111_1111).into_iter() {
+            bits.push(v);
+        }
         assert_eq!(bits, [1, 2, 4, 8, 16, 32, 64, 128, 256]);
     }
 
