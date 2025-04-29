abf5ce6f1dc56b9854420928e882885f48654cd3
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2025-04-29 17:15:32 +0900

    modified:   Makefile 	modified:   make_env.ps1 	modified:   make_env.sh 	deleted:    mypy.ini 	modified:   pyproject.toml 	new file:   ruff.toml 	new file:   uv.lock

--- make_env.ps1
+++ make_env.ps1
@@ -1,5 +1,7 @@
-python -m venv .venv
+uv python install 3.12
+uv python pin 3.12
+uv venv
 ./.venv/Scripts/activate
-pip install numpy pytest
+uv add numpy pytest ruff ipykernel
 cargo install cargo-llvm-cov
 rustup component add llvm-tools-preview
