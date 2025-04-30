c52d0916010d26ec48c516046c4fad1738978193
yiwaki <yasushi.iwaki@gmail.com>
2025-04-30 20:04:01 +0900

    modified:   make_env.ps1 	modified:   make_env.sh 	modified:   pyproject.toml 	modified:   uv.lock

--- make_env.ps1
+++ make_env.ps1
@@ -3,6 +3,6 @@ uv python pin 3.12
 uv venv
 ./.venv/Scripts/activate
 uv tool install maturin
-uv add numpy pytest ruff ipykernel
+uv sync
 cargo install cargo-llvm-cov
 rustup component add llvm-tools-preview
--- make_env.sh
+++ make_env.sh
@@ -3,6 +3,6 @@ uv python pin 3.12
 uv venv
 source ./.venv/bin/activate
 uv tool install maturin
-uv add numpy pytest ruff
+uv sync
 cargo install cargo-llvm-cov
 rustup component add llvm-tools-preview
