8e429e9f471c29fb800ca9c36d6ed3885c1036d2
yiwaki <yasushi.iwaki@gmail.com>
2025-05-01 19:34:43 +0900

    modified:   Cargo.toml 	modified:   make_env.sh 	modified:   ruff.toml 	modified:   sample.py 	new file:   sudoku_rust.pyi

--- make_env.sh
+++ make_env.sh
@@ -1,8 +1,7 @@
 uv python install 3.12
 uv python pin 3.12
-uv venv
+uv sync
 source ./.venv/bin/activate
 uv tool install maturin
-uv sync
 cargo install cargo-llvm-cov
 rustup component add llvm-tools-preview
