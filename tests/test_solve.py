a5c62c13e7107b3def0390821bd6c4450943be6d
Yasushi Iwaki <yasushi.iwaki@gmail.com>
2026-04-09 22:11:50 +0900

    pytestの追加

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -1,13 +1,33 @@
 import numpy as np
+import pytest
 
 from sudoku_rust import solve, check
 
 
-def test_solve():
+def test_solve_success():
     filepath = "data/evil_3.csv"
     x = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
 
     y = solve(x)
 
     assert check(y)
-    assert np.all((x == y) == (x != 0))
+
+
+def test_solve_fail_1():
+    filepath = "data/bad.csv"
+    x = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
+
+    with pytest.raises(ValueError) as e:
+        y = solve(x)
+
+    assert str(e.value) == "No solution found for the given Sudoku problem."
+
+
+def test_solve_fail_2():
+    filepath = "data/badbad.csv"
+    x = np.loadtxt(filepath, delimiter=",").astype(np.uint16)
+
+    with pytest.raises(ValueError) as e:
+        y = solve(x)
+
+    assert str(e.value) == "Input array must be of shape (9, 9)"
